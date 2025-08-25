f903acea69bdd096364d9781d875741ee04c09c8
sars2811 <sarthak28200111@gmail.com>
2025-08-26 02:12:23 +0530

    init: Added ruff linter and formatter.

--- tasks_to_habit/wsgi.py
+++ tasks_to_habit/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasks_to_habit.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tasks_to_habit.settings")
 
 application = get_wsgi_application()
